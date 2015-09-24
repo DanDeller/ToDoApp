512279fac9cd797c5dfa523224156310bf89ecb4
deller26 <deller26@gmail.com>
2015-09-24 13:31:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(express.static(__dirname + '/views'));
 
 // middlewear to check path and method
 app.use(function(req,res,next) {
-  console.log(req.path)
-  console.log(req.method);
-  next();
+	console.log(req.path)
+	console.log(req.method);
+  	next();
 });
 
 // get index DON'T NEED THIS! FIND OUT WHY!
@@ -29,37 +29,37 @@ app.use(function(req,res,next) {
 
 // get all tasks
 app.get('/tasks', function(req, res) {
-    r.table('tasks').run().then(function(tasks) {
+	r.table('tasks').run().then(function(tasks) {
     		var todo = tasks;
     		res.json(200, tasks);
-    }).error(function() {
-    		console.log('Something with the GET is broken...');
-    });
+	}).error(function() {
+	    	console.log('Something with the GET is broken...');
+	});
 });
 
 // post new tasks
 app.post('/tasks', function(req, res) {
 	var currentTask = req.body;
 	r.table('tasks').insert({name: currentTask.name, task: currentTask.task}).run(function() {
-        res.redirect('/');
-    });
+        	res.redirect('/');
+	});
 });
 
 // update tasks
 app.patch('/tasks', function(req, res) {
-    var query = _.extend(req.body,req.params,req.query);
-    var id = query.id;
-  r.table('tasks').get(id).update(query).run().then(function(tasks) {
-    res.send(tasks);
-  });
+	var query = _.extend(req.body,req.params,req.query);
+	var id = query.id;
+ 	r.table('tasks').get(id).update(query).run().then(function(tasks) {
+ 		res.send(tasks);
+  	});
 });
 
 // delete tasks
 app.delete('/tasks', function(req, res) {
-    var currentId = req.query;
-    r.table('tasks').get(currentId.id).delete().run().then(function(tasks) {
-        res.json(200, tasks);
-    });
+	var currentId = req.query;
+	r.table('tasks').get(currentId.id).delete().run().then(function(tasks) {
+        	res.json(200, tasks);
+    	});
 });
 
 // start up our server
@@ -68,4 +68,3 @@ var server = app.listen(3000, function() {
 	var host = server.address().address;
 	console.log('Example app listening at http://%s:%s', host, port);
 });
-
