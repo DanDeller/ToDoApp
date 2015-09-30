550f39b3fe76de2ad47eafc754f4f15e877106ac
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2015-09-30 15:51:46 -0700

    updates

--- app.js
+++ app.js
@@ -14,33 +14,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/views'));
 
-// middlewear to check path and method
-// app.use(function(req,res,next) {
-//   console.log(req.path)
-//   console.log(req.method);
-//   next();
-// });
+// middleware
+app.use(function(req,res,next) {
+  // console.log(req.path)
+  // console.log(req.method);
+  next();
+});
 
 // get index DON'T NEED THIS! FIND OUT WHY!
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/views/index.html'));
-// });
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 
 // get all tasks
 app.get('/tasks', function(req, res) {
-    r.table('tasks').run().then(function(tasks) {
-    		var todo = tasks;
-    		res.json(200, tasks);
-    }).error(function() {
-    		console.log('Something with the GET is broken...');
+    r.table('tasks').run(function(err, cursor) {
+      var todo = cursor;
+        res.json(200, cursor);
+      }).error(function() {
+    		console.log('Something with the GET method is broken.');
     });
 });
 
 // post new tasks
 app.post('/tasks', function(req, res) {
 	var currentTask = req.body;
-	r.table('tasks').insert({name: currentTask.name, task: currentTask.task}).run(function(err, cursor) {
+	r.table('tasks').insert({
+        name: currentTask.name,
+        task: currentTask.task
+    }).run(function(err, cursor) {
         res.redirect('/');
+    }).error(function() {
+      console.log('Something with the POST method is broken.');
     });
 });
 
@@ -48,16 +53,20 @@ app.post('/tasks', function(req, res) {
 app.patch('/tasks', function(req, res) {
     var query = _.extend(req.body,req.params,req.query);
     var id = query.id;
-  r.table('tasks').get(id).update(query).run().then(function(tasks) {
-    res.send(tasks);
-  });
+    r.table('tasks').get(id).update(query).run(function(err, cursor) {
+      res.send(cursor);
+    }).error(function() {
+      console.log('Something with the PATCH method is broken.');
+    });
 });
 
 // delete tasks
 app.delete('/tasks', function(req, res) {
     var currentId = req.query;
-    r.table('tasks').get(currentId.id).delete().run().then(function(tasks) {
-        res.json(200, tasks);
+    r.table('tasks').get(currentId.id).delete().run(function(err, cursor) {
+      res.json(200, cursor);
+    }).error(function() {
+      console.log('Something with the DELETE method is broken.');
     });
 });
 
