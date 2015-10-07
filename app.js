295f2d5b7e51d8ca6ca3e1abc8c081affcaf1f2e
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2015-10-07 16:07:32 -0700

    getting routing setup

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ var router  = express.Router();
 var path    = require('path');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
+var requireDir = require('require-dir');
 var r = require('rethinkdbdash')({
 	host: 'localhost',
 	port: 28015,
 	db: 'testSite'
 });
+
+var endpoints = requireDir('./lib/endpoints');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,53 +24,11 @@ app.use(function(req,res,next) {
   next();
 });
 
+app.use(endpoints.tasks);
+
 // get index DON'T NEED THIS! FIND OUT WHY!
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/views/index.html'));
-});
-
-// get all tasks
-app.get('/tasks', function(req, res) {
-    r.table('tasks').run(function(err, cursor) {
-      var todo = cursor;
-        res.json(200, cursor);
-      }).error(function() {
-    		console.log('Something with the GET method is broken.');
-    });
-});
-
-// post new tasks
-app.post('/tasks', function(req, res) {
-	var currentTask = req.body;
-	r.table('tasks').insert({
-        name: currentTask.name,
-        task: currentTask.task
-    }).run(function(err, cursor) {
-        res.redirect('/');
-    }).error(function() {
-      console.log('Something with the POST method is broken.');
-    });
-});
-
-// update tasks
-app.patch('/tasks', function(req, res) {
-    var query = _.extend(req.body,req.params,req.query);
-    var id = query.id;
-    r.table('tasks').get(id).update(query).run(function(err, cursor) {
-      res.send(cursor);
-    }).error(function() {
-      console.log('Something with the PATCH method is broken.');
-    });
-});
-
-// delete tasks
-app.delete('/tasks', function(req, res) {
-    var currentId = req.query;
-    r.table('tasks').get(currentId.id).delete().run(function(err, cursor) {
-      res.json(200, cursor);
-    }).error(function() {
-      console.log('Something with the DELETE method is broken.');
-    });
 });
 
 // start up our server
--- test/test.js
+++ test/test.js
@@ -1,28 +1,32 @@
 var assert = require("chai").assert;
 var request = require('supertest');
+var should = require('should');
+var assert = require('assert');
 var app = require('../app.js');
 
 
 describe('api', function() {
   describe('task', function () {
-    it('should return 200 on get request', function (done) {
+
+    it('should return 200 on GET', function (done) {
       request(app)
       .get('/tasks')
-      .expect(200, done);
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
     });
-    it('should return 200 on delete request', function (done) {
-      request(app)
-      .del('/tasks')
-      .expect(200, done);
-    });
+
+    // it('should return 200 on DELETE', function (done) {
+    //   request(app)
+    //   .delete('/tasks/?id=' + currentId)
+    //   .expect(200)
+    //   .end(function(err, res) {
+    //     if (err) return done(err);
+    //     done();
+    //   });
+    // });
+
   });
-
-  // describe('users', function () {
-  //   it('should return 200 on get request', function (done) {
-  //     request(app)
-  //     .get('/tasks')
-  //     .expect(200, done);
-  //   });
-  // });
-
 });
