d8ab4f464584e0956ebe0be66fac273c0350c509
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2015-10-22 18:30:38 -0700

    working on directive to display tasks on page

--- public/app.js
+++ public/app.js
@@ -6,16 +6,19 @@ scotchApp.config(function($routeProvider) {
         templateUrl : 'views/login.html',
         controller  : 'mainController'
     })
-    .when('/todo', {
+    .when('/tasks', {
         templateUrl : 'views/todo.html',
-        controller  : 'todoController'
+        controller  : 'tasksController'
     });
 });
 
-scotchApp.controller('mainController', function($scope) {
+scotchApp.controller('mainController', ['$scope', '$window', function($scope, $window) {
     $scope.message = 'home';
-});
+}]);
 
-scotchApp.controller('todoController', function($scope) {
-    $scope.message = 'todo';
+scotchApp.controller('tasksController', function($scope) {
+    $scope.message = 'tasks';
+
+    // jquery lite to grab tasks will need to go here
+
 });
