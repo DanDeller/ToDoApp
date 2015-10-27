54a885d28a264084b0158e179af6b0dd46adce21
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2015-10-27 15:50:38 -0700

    returning task object to page

--- public/app.js
+++ public/app.js
@@ -16,8 +16,22 @@ scotchApp.controller('mainController', ['$scope', '$window', function($scope, $w
     $scope.message = 'home';
 }]);
 
-scotchApp.controller('tasksController', function($scope) {
+scotchApp.controller('tasksController', function($scope, $http) {
     $scope.message = 'tasks';
+
+    var test = [];
+
+    $http.get('/tasks')
+        .success(function(data) {
+            $scope.tasks = data;
+            angular.forEach(data, function(value, key) {
+                this.push(key + value)
+            }, test);
+            console.log(test);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
 
     // jquery lite to grab tasks will need to go here
 
