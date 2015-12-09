237b10a2c423ef5f8e1e5d19a0a5225fee79f0d8
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2015-12-09 15:22:10 -0800

    working on logic to create rethinkdb tables

--- public/assets/mainJS/main.js
+++ public/assets/mainJS/main.js
@@ -14,6 +14,70 @@ danApp.config(function($routeProvider) {
     redirectTo : '/'
   });
 });
+danApp.controller('mainController', ['$scope', '$window', function mainController($scope, $window) {
+	
+}]);
+/**
+* @name tasksController
+* @description create task, read task, update task, delete task
+* @param task object, id, name, task
+*/
+danApp.controller('tasksController', ['$scope', '$http', 'taskService', 'taskFactory', function tasksController($scope, $http, taskService, taskFactory) {
+
+  $scope.tasks = [];
+  $scope.userImage = [];
+
+  $scope.getImage = function() {
+    taskService.getImage()
+    .then(function(image) {
+      $scope.useImage(image);
+    }, function(error) {
+      alert('Failed to get user image:' + error);
+    });
+  };
+
+  $scope.useImage = function(image) {
+    $scope.userImage = image;
+    console.log($scope.userImage);
+  }
+
+  $scope.getImage();
+
+  $scope.getAllTasks = function() {
+    taskService.readTasks()
+      .then(function(tasks) {
+         $scope.applyAllTasks(tasks);
+      }, function(error) {
+        alert('Read failed due to:' + error);
+      });
+  };
+
+  $scope.applyAllTasks = function(tasks) {
+    $scope.tasks = tasks;
+  };
+
+  $scope.getAllTasks();
+
+  $scope.createTask = function(name, task) {
+    taskService.createTask(name, task);
+    $scope.name = '';
+    $scope.task = '';
+  };
+
+  $scope.patchTask = function(id, name, task) {
+    taskService.updateTask(id, name, task);
+  };
+
+  $scope.destroyTask = function(id, index) {
+    $scope.tasks.splice(index, 1);
+    taskService.deleteTask(id);
+  };
+
+  $scope.focusOnIt = function() {
+    taskFactory.focusIt('focus');
+  };
+
+}]);
 /**
 * @name eventFocus
 * @description after adding a task focus on create task input
@@ -104,70 +168,6 @@ danApp.directive('deleteImage', function(taskService) {
 		}
 	}
 });
-danApp.controller('mainController', ['$scope', '$window', function mainController($scope, $window) {
-	
-}]);
-/**
-* @name tasksController
-* @description create task, read task, update task, delete task
-* @param task object, id, name, task
-*/
-danApp.controller('tasksController', ['$scope', '$http', 'taskService', 'taskFactory', function tasksController($scope, $http, taskService, taskFactory) {
-
-  $scope.tasks = [];
-  $scope.userImage = [];
-
-  $scope.getImage = function() {
-    taskService.getImage()
-    .then(function(image) {
-      $scope.useImage(image);
-    }, function(error) {
-      alert('Failed to get user image:' + error);
-    });
-  };
-
-  $scope.useImage = function(image) {
-    $scope.userImage = image;
-    console.log($scope.userImage);
-  }
-
-  $scope.getImage();
-
-  $scope.getAllTasks = function() {
-    taskService.readTasks()
-      .then(function(tasks) {
-         $scope.applyAllTasks(tasks);
-      }, function(error) {
-        alert('Read failed due to:' + error);
-      });
-  };
-
-  $scope.applyAllTasks = function(tasks) {
-    $scope.tasks = tasks;
-  };
-
-  $scope.getAllTasks();
-
-  $scope.createTask = function(name, task) {
-    taskService.createTask(name, task);
-    $scope.name = '';
-    $scope.task = '';
-  };
-
-  $scope.patchTask = function(id, name, task) {
-    taskService.updateTask(id, name, task);
-  };
-
-  $scope.destroyTask = function(id, index) {
-    $scope.tasks.splice(index, 1);
-    taskService.deleteTask(id);
-  };
-
-  $scope.focusOnIt = function() {
-    taskFactory.focusIt('focus');
-  };
-
-}]);
 /**
 * @name taskFactory
 * @description timeout for focus event/get tasks/set tasks
