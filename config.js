d5a82b6a8f4aa7a3b59a19d34ef65fb9153c303c
Daniel Deller <deller26@Daniels-MacBook-Pro.local>
2015-12-16 18:08:15 -0800

    checks for db and tables and creates if needed

--- config.js
+++ config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    'secret': 'your secret here'
+    'secret': 'your secret here',
+    rethinkdb: {
+    host: 'localhost',
+    port: 28015,
+    authKey: '',
+    db: 'todoApp'
+  },
+  express: {
+     port: 3000
+  }
 };
