ae88194f998894d43591b847cadd1611ba327868
Dan Deller <dandeller@Dan-Deller-MacBook-Pro.local>
2016-01-04 15:34:15 -0800

    updates to tests for client

--- test/test.js
+++ test/test.js
@@ -17,5 +17,35 @@ describe('api', function() {
       });
     });
 
+    it('should return 200 on POST', function (done) {
+      request(app)
+      .post('/tasks')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    it('should return 200 on PATCH', function (done) {
+      request(app)
+      .patch('/tasks')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
+    it('should return 200 on DELETE', function (done) {
+      request(app)
+      .delete('/tasks')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        done();
+      });
+    });
+
   });
 });
